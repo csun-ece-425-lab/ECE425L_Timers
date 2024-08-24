ae30ebe6f954e7d15764f8ecb24eb7aee9affb99
ananas-csun <ananas.ece25@gmail.com>
2024-08-24 01:26:41 -0700

    Added project files for the Timer lab

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ECE425L_Timers
+# ECE 425L - Microprocessor Systems Lab
+**CSU Northridge**
+
+**Department of Electrical and Computer Engineering**
+
+## Timers Lab
+The Timers lab interfaces with the following:
+- EduBase Board Push Buttons (SW2 - SW5)
+- EduBase Board LEDs (LED0 - LED3)
+- EduBase Board Seven-Segment Display
---
+++ Timers/main.c
@@ -0,0 +1,29 @@
+/*
+ * @file main.c
+ *
+ * @brief Main source code for the Timers program.
+ *
+ * This file contains the main entry point and function definitions for the Timers program.
+ * It interfaces with the following:
+ *	- EduBase Board LEDs (LED0 - LED3)
+ *	- EduBase Board Push Buttons (SW2 - SW5)
+ *	- EduBase Board Seven-Segment Display
+ *
+ * The following timers are used to demonstrate periodic interrupts: Timer 0A and Timer 1A.
+ *
+ * To verify the pinout of the user LED, refer to the Tiva C Series TM4C123G LaunchPad User's Guide
+ * Link: https://www.ti.com/lit/pdf/spmu296
+ *
+ * @author Aaron Nanas
+ */
+ 
+#include "TM4C123GH6PM.h"
+
+int main(void)
+{
+
+	while(1)
+	{
+
+	}
+}
